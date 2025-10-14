03f5509c8d4d1f11d408991fd870a5511c7abc86
LuizHerni16 <lbastos722@gmail.com>
2025-10-14 16:12:08 -0300

    Commit inicial do projeto

---
+++ src/main/java/com/fitzone/FITZONE/FitzoneApiApplication.java
@@ -0,0 +1,13 @@
+package com.fitzone.FITZONE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitzoneApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitzoneApiApplication.class, args);
+	}
+
+}
